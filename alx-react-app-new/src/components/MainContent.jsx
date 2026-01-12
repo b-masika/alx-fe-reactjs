eee711ef0c4fbffb2100c5ef8e8fc87bb6f8bc69
Bob <bobmasika@gmail.com>
2026-01-12 21:34:21 +0300

    Feat: Styling Main Content

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,14 @@
-function MainContent() {
+// src/components/MainContent.jsx
+import React from 'react'
+
+const MainContent = () => {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ padding: '20px', backgroundColor: '#f0f0f0', minHeight: '400px'}}>
+            <p style={{ fontSize: '18px', lineHeight: '1.6'}}>
+                I love to visit New York, Paris, and Tokyo.
+            </p>
         </main>
-    );
+    )
 }
 
 export default MainContent;
