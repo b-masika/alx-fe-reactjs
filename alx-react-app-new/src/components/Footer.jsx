f853bdfb25b51a6d47da0e9111f9b8dba8a45ec6
Bob <bobmasika@gmail.com>
2026-01-12 21:40:41 +0300

    feat: Styling Footer

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,12 @@
-function Footer() {
+// src/components/Footer.jsx
+import React from 'react'
+
+const Footer = () => {
     return (
-        <footer>
+        <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px'}}>
             <p>© 2023 City Lovers</p>
         </footer>
-    );
+    )
 }
 
 export default Footer;
