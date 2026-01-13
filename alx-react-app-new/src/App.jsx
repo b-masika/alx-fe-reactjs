80314fd4fcd526941608979cc2f4837a002d1dd2
Bob <bobmasika@gmail.com>
2026-01-13 19:37:19 +0300

    feat: Create a Simple Application using State and Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,12 +5,16 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 
+// import the new Counter component
+import Counter from './components/Counter';
+
 function App() {
   return (
     <div>
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <Counter/>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </div>
