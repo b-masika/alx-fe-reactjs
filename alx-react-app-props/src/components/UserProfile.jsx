4a8deb97031e1274ba2f351018ba1e0d01adbd0a
Bob <bobmasika@gmail.com>
2026-01-13 20:41:03 +0300

    UserContext Edits

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,16 @@
 // src/components/UserProfile.jsx
-const UserProfile = (props) => {
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+const UserProfile = () => {
+    // Consume data directly
+    const userData = useContext(UserContext);
+
     return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-            <h2 style={{ color: 'blue' }}>{props.name}</h2>
-            <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-            <p>Bio: {props.bio}</p>
+            <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{UserData.age}</span></p>
+            <p>Bio: {userData.bio}</p>
         </div>
     );
 };
