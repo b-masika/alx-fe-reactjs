f23344abb59c48e2199291bf991f2b4777b164bc
Bob <bobmasika@gmail.com>
2026-01-31 14:32:57 +0300

    Final Changes

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,8 +2,14 @@ import axios from 'axios';
 
 const SEARCH_API =  'https://api.github.com/search/users?q';
 
+const token = import.meta.env.REACT_APP_GITHUB_TOKEN;
+
+const headers = token ? {
+    Authorization: `token ${token}`
+} : {};
+
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await axios.get(`https://api.github.com/users/${username}`, {headers});
     return response.data;
 }
 
@@ -12,6 +18,6 @@ export const searchUsers = async ({query, location, minRepos, page = 1}) => {
     if (location) q += ` location:${location}`;
     if (minRepos) q += ` repos:>=${minRepos}`;
          
-    const response = await axios.get(`${SEARCH_API}=${q}&page=${page}`);
+    const response = await axios.get(`${SEARCH_API}=${encodeURIComponent(q)}&page=${page}`, {headers});
     return response.data.items;
 };
