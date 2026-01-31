9e4673ae8536f393ab2a001572f33b468c3e190d
Bob <bobmasika@gmail.com>
2026-01-31 09:00:00 +0300

    Extend Github Service to support advanced user search queries

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -8,3 +8,23 @@ export const fetchUserData = async (username) => {
     const response = await githubApi.get(`/users/${username}`);
     return response.data;
 }
+
+export const searchUsers = async ({query, location, minRepos, page = 1}) => {
+    let searchQuery = query || "users";
+
+    if (location) {
+        searchQuery += ` location:${location}`;
+    }
+
+    if (minRepos) {
+        searchQuery += ` repos:>=${minRepos}`;
+    }
+    const response = await githubApi.get('/search/users', {
+        params: {
+            q: searchQuery,
+            page,
+            per_page: 10,
+        },
+    });
+    return response.data.items;
+};
